912dcad94bcdc5f1a51a68f4ebc27e627d9a0c90
bonk-dev <github.p0j6t@passinbox.com>
2024-06-17 19:21:26 +0200

    Add exception codes and Modbus exception class

---
+++ dotbus/Exceptions/ModbusException.cs
@@ -0,0 +1,16 @@
+using dotbus.Modbus;
+
+namespace dotbus.Exceptions;
+
+public class ModbusException : Exception
+{
+    public EExceptionCode ExceptionCode { get; }
+
+    public override string Message => $"The Modbus slave device returned an exception: ${Enum.GetName(ExceptionCode)}. " +
+                                      $"Lookup the Modbus Application Protocol V1.1 for more details.";
+
+    public ModbusException(EExceptionCode exceptionCode)
+    {
+        ExceptionCode = exceptionCode;
+    }
+}
