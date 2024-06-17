2a9ec1b516262fe4edee6397c897392a8446d29e
bonk-dev <github.p0j6t@passinbox.com>
2024-06-17 15:22:03 +0200

    Read IP from args

--- dotbus.cli/Program.cs
+++ dotbus.cli/Program.cs
@@ -8,11 +8,11 @@ var client = new TcpClient()
     NoDelay = true
 };
 
-await client.ConnectAsync("192.168.2.110", 502);
+await client.ConnectAsync(args[0], 502);
 var stream = client.GetStream();
 
 await using var mClient = new ModbusTcpClient(stream, 0x01);
-for (var i = 0; i < 1000000; ++i)
+for (var i = 0; i < 10000; ++i)
 {
     await mClient.ReadCoilsAsync(0, ReadCoilsRequest.MaxCoilAmount);
     // mClient.ReadCoils(0, ReadCoilsRequest.MaxCoilAmount); 
