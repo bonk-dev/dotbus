8035c4db468bb1cee2e86ce0787474332cd9ff53
bonk-dev <github.p0j6t@passinbox.com>
2024-06-17 19:28:58 +0200

    Remove the $ from the message string

--- dotbus/Exceptions/ModbusException.cs
+++ dotbus/Exceptions/ModbusException.cs
@@ -6,7 +6,7 @@ public class ModbusException : Exception
 {
     public EExceptionCode ExceptionCode { get; }
 
-    public override string Message => $"The Modbus slave device returned an exception: ${Enum.GetName(ExceptionCode)}. " +
+    public override string Message => $"The Modbus slave device returned an exception: {Enum.GetName(ExceptionCode)}. " +
                                       $"Lookup the Modbus Application Protocol V1.1 for more details.";
 
     public ModbusException(EExceptionCode exceptionCode)
